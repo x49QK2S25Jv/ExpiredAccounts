4bf1239b0eaa5ad242cfc8194e9315253f9a4b7d
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-12 12:54:15 +1300

    Update AccountsExpired.ps1

--- AccountsExpired.ps1
+++ AccountsExpired.ps1
@@ -7,7 +7,8 @@ $ExpiringAccountDaysRequested = Read-Host -Prompt "Please tell me how many days 
 
 #Task one.b menu
 
-
+cls
+sleep 1
 #Task two, ask for expired only
 $ExpiredAccounts = Search-ADAccount -AccountExpired | Format-Table Name,LastLogonDate,AccountExpirationDate -AutoSize | Out-String
 
@@ -17,7 +18,21 @@ $ExpiringAccounts = Search-ADAccount -AccountExpiring | Format-Table Name -AutoS
 
 
 #Task four show expired and expiring
+if ($ExpiredAccounts -eq $null){
+Write-Host "No accounts have expired" -ForegroundColor Green
+}
+else
+{
 Write-Host "The below accounts have expired" -ForegroundColor Magenta
-Write-Host "$ExpiredAccounts.Name $ExpiredAccounts.LastLogonDate"
+Write-Host "$ExpiredAccounts"
+}
+
+
+if ($ExpiredAccounts -eq $null)
+{
+Write-Host "No accounts are going to expire within $ExpiringAccountDaysRequested" day/s -ForegroundColor Green}
+else
+{
 Write-Host "The below accounts are expiring soon" -ForegroundColor Magenta
 Write-Host "$ExpiringAccounts"
+}
