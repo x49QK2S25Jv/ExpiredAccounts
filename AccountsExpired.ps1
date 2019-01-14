13467d598e72f8f0a0ac072a18238c8baa91b66a
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-14 14:41:24 +1300

    Update AccountsExpired.ps1

--- AccountsExpired.ps1
+++ AccountsExpired.ps1
@@ -1,10 +1,10 @@
-﻿{#Task one Variables
+﻿#Task one Variables
 $TodaysDate = (Get-Date -Format dd/MM/yyyy)
 Clear-Host
 $ExpiredAccounts = Search-ADAccount -AccountExpired | Format-Table Name,LastLogonDate,AccountExpirationDate -AutoSize | Out-String
 $ExpiringAccounts = Search-ADAccount -AccountExpiring | Format-Table Name,LastLogonDate,AccountExpirationDate -AutoSize | Out-String
 
-Write-Host (Script is not required to be run as "administrator" or domain admin) -ForegroundColor Green
+#Write-Host (Script is not required to be run as "administrator" or domain admin) -ForegroundColor Green
 $menu=@"
 1 Show accounts which have expired 
 2 Show accounts which are expiring soon
@@ -12,6 +12,7 @@ $menu=@"
 Q Quit
 Select a task by number or Q to quit
 "@
+
 $r = Read-Host $menu
 
 Switch ($r) {
@@ -27,16 +28,10 @@ Write-Host "$ExpiredAccounts"
 }}
  
 "2" {
-$DateRequestedMsg = Write-host "Please date dateback as dd/MM/yyyy" -ForegroundColor Red
+$DateRequestedMsg = Write-host "Please back date as dd/MM/yyyy" -ForegroundColor Red
 if ($ExpiringAccounts -eq ""){
-$ExpiringAccountDaysRequested = Read-Host -Prompt "Show accounts which will expire in the next X $Days"
-$Days = if ($CurrentDate -le $TodaysDate -and $CurrentDate -le 2) {
-Write-Host = "day" | Out-String
-}
+$ExpiringAccountDaysRequested = Read-Host -Prompt "Show accounts which will expire in the next X day/s"
 else{
-Write-Host = "days" | Out-String
-}
-
 Write-Host "No accounts are going to expire within $ExpiringAccountDaysRequested $Days" -ForegroundColor Green
 else
 {
@@ -44,7 +39,7 @@ Write-Host "The below accounts are expiring soon" -ForegroundColor Magenta
 Write-Host "$ExpiringAccounts"
     }
         }
-            }
+            } 
 "3"{
 if ($ExpiredAccounts -eq ""){
 $ExpiringAccountDaysRequested = Read-Host -Prompt "Show accounts which will expire in the next"
