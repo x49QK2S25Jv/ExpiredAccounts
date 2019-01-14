7325c8386638a0f6c17d8afaac2d5b15e0c9e506
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-14 13:57:51 +1300

    Update AccountsExpired.ps1

--- AccountsExpired.ps1
+++ AccountsExpired.ps1
@@ -1,9 +1,11 @@
-﻿#Task one Variables
+﻿{#Task one Variables
 $TodaysDate = (Get-Date -Format dd/MM/yyyy)
-cls
+Clear-Host
 $ExpiredAccounts = Search-ADAccount -AccountExpired | Format-Table Name,LastLogonDate,AccountExpirationDate -AutoSize | Out-String
 $ExpiringAccounts = Search-ADAccount -AccountExpiring | Format-Table Name,LastLogonDate,AccountExpirationDate -AutoSize | Out-String
+}
 
+Write-Host "Script is not required to be run as"administrator" or domain admin" -ForegroundColor Green
 $menu=@"
 1 Show accounts which have expired 
 2 Show accounts which are expiring soon
@@ -12,9 +14,7 @@ Q Quit
  
 Select a task by number or Q to quit
 "@
-
 $r = Read-Host $menu
-
 
 Switch ($r) {
 #Task two, ask for expired only
@@ -29,10 +29,8 @@ Write-Host "$ExpiredAccounts"
 }}
  
 "2" {
+$DateRequestedMsg = Write-host "Please date dateback as dd/MM/yyyy" -ForegroundColor Red
 if ($ExpiringAccounts -eq ""){
-$DateRequestedMsg = Write-host "Please enter date as dd/MM/yyyy"
-
-
 $ExpiringAccountDaysRequested = Read-Host -Prompt "Show accounts which will expire in the next $Days"
 $Days = if ($CurrentDate -le $TodaysDate -and $CurrentDate -le 2) {
 Write-Host = "day" | Out-String
@@ -40,7 +38,6 @@ Write-Host = "day" | Out-String
 else{
 Write-Host = "days" | Out-String
 }
-
 
 Write-Host "No accounts are going to expire within $ExpiringAccountDaysRequested $Days" -ForegroundColor Green
 else
