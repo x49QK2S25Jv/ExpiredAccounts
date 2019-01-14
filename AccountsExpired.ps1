95cf99356dabb033443ce1e72c87d5ac28ff49c1
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-14 14:06:57 +1300

    Update AccountsExpired.ps1

--- AccountsExpired.ps1
+++ AccountsExpired.ps1
@@ -4,7 +4,7 @@ Clear-Host
 $ExpiredAccounts = Search-ADAccount -AccountExpired | Format-Table Name,LastLogonDate,AccountExpirationDate -AutoSize | Out-String
 $ExpiringAccounts = Search-ADAccount -AccountExpiring | Format-Table Name,LastLogonDate,AccountExpirationDate -AutoSize | Out-String
 
-Write-Host "Script is not required to be run as"administrator" or domain admin" -ForegroundColor Green
+Write-Host (Script is not required to be run as "administrator" or domain admin) -ForegroundColor Green
 $menu=@"
 1 Show accounts which have expired 
 2 Show accounts which are expiring soon
@@ -29,7 +29,7 @@ Write-Host "$ExpiredAccounts"
 "2" {
 $DateRequestedMsg = Write-host "Please date dateback as dd/MM/yyyy" -ForegroundColor Red
 if ($ExpiringAccounts -eq ""){
-$ExpiringAccountDaysRequested = Read-Host -Prompt "Show accounts which will expire in the next $Days"
+$ExpiringAccountDaysRequested = Read-Host -Prompt "Show accounts which will expire in the next X $Days"
 $Days = if ($CurrentDate -le $TodaysDate -and $CurrentDate -le 2) {
 Write-Host = "day" | Out-String
 }
@@ -42,9 +42,9 @@ else
 {
 Write-Host "The below accounts are expiring soon" -ForegroundColor Magenta
 Write-Host "$ExpiringAccounts"
-}}
-
-}
+    }
+        }
+            }
 "3"{
 if ($ExpiredAccounts -eq ""){
 $ExpiringAccountDaysRequested = Read-Host -Prompt "Show accounts which will expire in the next"
